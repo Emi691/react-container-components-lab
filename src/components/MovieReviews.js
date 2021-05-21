5f0f288c7f86277a9b23cbe2d6dd384ccb669796
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-20 23:21:34 -0700

    commit tree 0b2c289b262e084ca6fd237a5427cf428dc0dcb1

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+    return (
+    <div className= 'review-list'>
+        {reviews.map(review => <div className = 'review'>{review.display_title}{review.mpaa_rating}{!!review.critics_pick}</div>)}
+    </div>
+    )
+}
+
+export default MovieReviews
